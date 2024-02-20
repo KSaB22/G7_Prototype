d7ae31fee485758250ca7682a859f1ae39406bed
KSaB22 <3backids@gmail.com>
2024-02-20 18:07:39 +0200

    init

---
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -0,0 +1,109 @@
+package il.cshaifasweng.OCSFMediatorExample.server;
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Message;
+import il.cshaifasweng.OCSFMediatorExample.server.ocsf.AbstractServer;
+import il.cshaifasweng.OCSFMediatorExample.server.ocsf.ConnectionToClient;
+import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class SimpleServer extends AbstractServer {
+	private static ArrayList<SubscribedClient> SubscribersList = new ArrayList<>();
+	private static String ids = "325345064"; //where I save the ID
+	private static String submitterName = "Kfir Back";	//my name
+
+
+	public SimpleServer(int port) {
+		super(port);
+		
+	}
+
+	@Override
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		Message message = (Message) msg;
+		String request = message.getMessage();
+		try {
+			//we got an empty message, so we will send back an error message with the error details.
+			if (request.isBlank()){
+				message.setMessage("Error! we got an empty message");
+				client.sendToClient(message);
+			}
+			//we got a request to change submitters IDs with the updated IDs at the end of the string, so we save
+			// the IDs at data field in Message entity and send back to all subscribed clients a request to update
+			//their IDs text fields. An example of use of observer design pattern.
+			//message format: "change submitters IDs: 123456789, 987654321"
+			else if(request.startsWith("change submitters IDs:")){
+				message.setData(request.substring(23));
+				ids = message.getData().substring(0,9);	//formatting the first id
+				if(message.getData().split(", ").length == 2){	// checking for a second id
+					ids +=  message.getData().substring(9,20);
+				}
+				message.setMessage("update submitters IDs");
+				sendToAllClients(message);
+			}
+			//we got a request to add a new client as a subscriber.
+			else if (request.equals("add client")){
+				SubscribedClient connection = new SubscribedClient(client);
+				SubscribersList.add(connection);
+				message.setMessage("client added successfully");
+				client.sendToClient(message);
+			}
+			//we got a message from client requesting to echo Hello, so we will send back to client Hello world!
+			else if(request.startsWith("echo Hello")){
+				message.setMessage("Hello World!");
+				client.sendToClient(message);
+			}
+			else if(request.startsWith("send Submitters IDs")){
+				message.setMessage(ids);
+				client.sendToClient(message);
+			}
+			else if (request.startsWith("send Submitters")){
+				message.setMessage(submitterName);
+				client.sendToClient(message);
+			}
+			else if (request.equals("what’s the time?")) {
+				DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+				Date date = new Date();
+				message.setMessage(dateFormat.format(date));
+				client.sendToClient(message);
+			}
+			else if (request.startsWith("multiply")){
+				int numN, numM;
+				numN = Integer.parseInt(request.substring(9).split("\\*")[0]);	//taking n from the message
+				numM = Integer.parseInt(request.substring(9).split("\\*")[1]);	//taking m from the message
+				message.setMessage(numN*numM + "");
+				client.sendToClient(message);
+			}else{
+				//add code here to send received message to all clients.
+				//The string we received in the message is the message we will send back to all clients subscribed.
+				//Example:
+					// message received: "Good morning"
+					// message sent: "Good morning"
+				//see code for changing submitters IDs for help
+				sendToAllClients(message);
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public void sendToAllClients(Message message) {
+		try {
+			for (SubscribedClient SubscribedClient : SubscribersList) {
+				SubscribedClient.getClient().sendToClient(message);
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
